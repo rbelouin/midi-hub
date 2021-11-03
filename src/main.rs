70735f26bcc5a1fbc91d51f5aab8f57d4ad0ee87
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2021-11-03 19:23:55 +0100

    List the MIDI devices connected

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate portmidi as pm;
+
+use portmidi::{DeviceInfo, PortMidi, Result};
+
 fn main() {
-    println!("Hello, world!");
+    let devices = devices();
+
+    if devices.len() == 0 {
+        println!("No devices!");
+    } else {
+        for device in devices {
+            println!("Device: {}", device);
+        }
+    }
+}
+
+fn devices() -> Vec<DeviceInfo> {
+    let context: Result<PortMidi> = pm::PortMidi::new();
+    let devices: Result<Vec<DeviceInfo>> = context.and_then(|c| c.devices());
+
+    return devices.unwrap_or_else(|err| {
+        println!("Error: {}", err);
+        return Vec::new();
+    });
 }
