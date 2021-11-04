e1fe20275818ec89d3e9de09a0d0c73362f1f50f
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2021-11-04 10:37:45 +0100

    Listen to the 1st event sent by the input device

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate portmidi as pm;
 
-use portmidi::{DeviceInfo, PortMidi, Result};
+use portmidi::{DeviceInfo, InputPort, MidiEvent, PortMidi, Result};
 use std::env;
 
 fn main() {
@@ -8,17 +8,30 @@ fn main() {
 
     match &args[1..] {
         [input_name, output_name] => {
-            let devices = devices();
+            match pm::PortMidi::new() {
+                Ok(context) => {
+                    let devices = devices(&context);
 
-            match select_devices(&devices, &input_name, &output_name) {
-                Some((input,  output)) => {
-                    in_separator(|| {
-                        println!("Selected MIDI devices:");
-                        println!("{}", input);
-                        println!("{}", output);
-                    });
+                    match select_devices(&devices, &input_name, &output_name) {
+                        Some((input,  output)) => {
+                            in_separator(|| {
+                                println!("Selected MIDI devices:");
+                                println!("{}", input);
+                                println!("{}", output);
+                            });
+
+                            match DeviceInfo::new(input.id()) {
+                                Ok(device) => {
+                                    let event = listen_to_device(&context, device);
+                                    println!("Event: {:?}", event);
+                                },
+                                _ => println!("Could not find MIDI device with id: {}", input.id()),
+                            }
+                        },
+                        _ => println!("No devices matching both the input name and output name")
+                    }
                 },
-                _ => println!("No devices matching both the input name and output name")
+                _ => println!("Could not initialize MIDI context"),
             }
         },
         _ => println!("Usage: ./midi-hub <input-name> <output-name>"),
@@ -31,9 +44,8 @@ fn in_separator<F: Fn()>(f: F) {
     println!("====================");
 }
 
-fn devices() -> Vec<DeviceInfo> {
-    let context: Result<PortMidi> = pm::PortMidi::new();
-    let devices: Result<Vec<DeviceInfo>> = context.and_then(|c| c.devices());
+fn devices(context: &PortMidi) -> Vec<DeviceInfo> {
+    let devices: Result<Vec<DeviceInfo>> = context.devices();
     let unwrapped_devices = devices.unwrap_or_else(|err| {
         println!("Error: {}", err);
         return Vec::new();
@@ -69,4 +81,15 @@ fn select_devices<'a>(devices: &'a Vec<DeviceInfo>, input_name: &'a str, output_
         (Some(input), Some(output)) => Some((input, output)),
         _ => None,
     };
+}
+
+fn listen_to_device(context: &PortMidi, device: DeviceInfo) -> MidiEvent {
+    let mut event: Option<MidiEvent> = None;
+    let ref mut input_port: Option<InputPort> = context.input_port(device, 1024).ok();
+
+    while event.is_none() {
+        event = input_port.as_mut().and_then(|port| port.read().ok().flatten());
+    }
+
+    return event.unwrap();
 }
