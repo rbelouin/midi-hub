60eba913e971c346c5bd31620d5e8c95e4fd568d
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2023-02-21 22:09:29 +0100

    midi: improve the configuration assistant

--- src/midi/devices/config.rs
+++ src/midi/devices/config.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
-use std::iter::Extend;
 
 use serde::{Serialize, Deserialize};
+
+use dialoguer::{theme::ColorfulTheme, Input, MultiSelect, Select};
+
+use crate::midi::Connections;
 
 pub type Config = HashMap<String, DeviceConfig>;
 
@@ -12,7 +15,7 @@ pub struct DeviceConfig {
     pub device_type: DeviceType,
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Copy, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum DeviceType {
     Default,
@@ -22,53 +25,50 @@ pub enum DeviceType {
 pub fn configure() -> Result<Config, Box<dyn std::error::Error>> {
     let mut config = Config::new();
 
-    let mut name = String::new();
-    let mut device_id = String::new();
-    let mut add_device = String::new();
+    let connections = Connections::new()?;
+    let device_names = connections.get_device_names();
 
-    println!("[midi] please enter the name of the MIDI device: ");
-    std::io::stdin().read_line(&mut name)?;
-    let name = name.trim().to_string();
-    println!("");
+    if device_names.is_empty() {
+        panic!("[midi] no devices found. Have you connected your MIDI devices before proceeding?");
+    }
 
-    println!("[midi] please enter the identifier you want to give to this device: ");
-    std::io::stdin().read_line(&mut device_id)?;
-    let device_id = device_id.trim().to_string();
-    println!("");
+    let mut selected_items = vec![];
 
-    let device_type = configure_type()?;
+    while selected_items.is_empty() {
+        selected_items = MultiSelect::with_theme(&ColorfulTheme::default())
+            .with_prompt("[midi] please select the MIDI devices you want to configure (use spacebar to select):")
+            .items(device_names.as_slice())
+            .interact()?;
+    }
 
-    config.insert(device_id, DeviceConfig {
-        name,
-        device_type,
-    });
+    for selected_item in selected_items {
+        let name = device_names[selected_item].trim().to_string();
+        let device_id: String = Input::with_theme(&ColorfulTheme::default())
+            .with_prompt("[midi] please enter the identifier you want to give to this device: ")
+            .interact_text()?;
 
-    println!("[midi] do you want to configure another device? (yes|no)");
-    std::io::stdin().read_line(&mut add_device)?;
-    let add_device = add_device.trim().to_string();
-    println!("");
+        let device_id = device_id.trim().to_string();
+        let device_type = configure_type(&name)?;
 
-    if add_device == "yes" {
-        config.extend(configure()?);
+        config.insert(device_id, DeviceConfig {
+            name,
+            device_type,
+        });
     }
 
     return Ok(config);
 }
 
-fn configure_type() -> Result<DeviceType, Box<dyn std::error::Error>> {
-    let mut device_type = String::new();
+fn configure_type(name: &String) -> Result<DeviceType, Box<dyn std::error::Error>> {
+    let device_types = vec![DeviceType::Default, DeviceType::LaunchpadPro];
+    let serialized_device_types = device_types.as_slice().into_iter()
+        .map(|t| format!("{:?}", t))
+        .collect::<Vec<String>>();
 
-    println!("[midi] please enter the type of the MIDI device (default|launchpadpro): ");
-    std::io::stdin().read_line(&mut device_type)?;
-    let device_type = device_type.trim().to_string();
-    println!("");
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt(format!("[midi] please select what the type of the device \"{}\" is (use spacebar to select):", name))
+        .items(serialized_device_types.as_slice())
+        .interact()?;
 
-    return match device_type.as_str() {
-        "default" => Ok(DeviceType::Default),
-        "launchpadpro" => Ok(DeviceType::LaunchpadPro),
-        _ => {
-            eprintln!("[midi] could not parse the device type: {}", device_type);
-            configure_type()
-        },
-    }
+    return Ok(device_types[selection]);
 }
